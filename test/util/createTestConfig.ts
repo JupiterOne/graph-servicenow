76f33ee131342917bfb0fdbb85c0d44fe758dc24
d10p3t <d10p3t@gmail.com>
2022-11-01 17:41:50 +0800

    minor fixes

--- test/util/createTestConfig.ts
+++ test/util/createTestConfig.ts
@@ -1,8 +1,6 @@
 import { IntegrationConfig } from '../../src/types';
 
 export function createTestConfig(hostname: string): IntegrationConfig {
-  console.log(JSON.stringify(process.env));
-
   return {
     hostname: process.env.HOSTNAME || hostname,
     username: process.env.USERID || 'default-username',
